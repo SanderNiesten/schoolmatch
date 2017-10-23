e4ad0530d5b459448dbbfbbfafe92eaaa471d487
sjki-l <sjkilouwerse@gmail.com>
2017-10-23 17:21:49 +0200

    Add User views, admin-rails and cancan

--- Gemfile
+++ Gemfile
@@ -59,3 +59,7 @@ gem 'devise', '~> 4.2'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'remotipart', github: 'JangoSteve/remotipart'
+gem 'rails_admin', '~> 1.2'
+gem "cancan"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'pages#home'
 
-  devise_for :users
+  devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      passwords: 'users/passwords',
+      registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
